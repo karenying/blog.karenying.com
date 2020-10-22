ee5169c7c2954194862712a5903db07443de6dcd
Karen Ying <karenying7@gmail.com>
2020-10-22 13:30:36 -0400

    Add material-ui and icons

--- src/components/Sidebar/Contacts/Contacts.js
+++ src/components/Sidebar/Contacts/Contacts.js
@@ -1,31 +1,73 @@
-// @flow strict
 import React from 'react';
 import { getContactHref, getIcon } from '../../../utils';
 import Icon from '../../Icon';
 import styles from './Contacts.module.scss';
 
-type Props = {
-  contacts: {
-    [string]: string,
-  },
-};
+import { FaLinkedin, FaCode } from 'react-icons/fa';
+import { AiFillTwitterCircle, AiFillMediumCircle } from 'react-icons/ai';
+import { GoMarkGithub } from 'react-icons/go';
+import { IoMdMail } from 'react-icons/io';
+import Tooltip from '@material-ui/core/Tooltip';
 
-const Contacts = ({ contacts }: Props) => (
+
+const Contacts = () => (
   <div className={styles['contacts']}>
-    <ul className={styles['contacts__list']}>
-      {Object.keys(contacts).map((name) => (!contacts[name] ? null : (
-        <li className={styles['contacts__list-item']} key={name}>
-          <a
-            className={styles['contacts__list-item-link']}
-            href={getContactHref(name, contacts[name])}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <Icon name={name} icon={getIcon(name)} />
-          </a>
-        </li>
-      )))}
-    </ul>
+    <div className={`${styles['contacts__code']} ${styles['contacts__link']}`}>
+      <Tooltip title="Site source code" aria-label="site source code" placement="top">
+        <a
+          href='https://github.com/karenying/blog.karenying.com'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaCode />
+        </a>
+      </Tooltip>
+
+    </div>
+    <div className={`${styles['contacts__github']} ${styles['contacts__link']}`}>
+      <Tooltip title="GitHub" aria-label="github" placement="top">
+        <a
+          href='https://github.com/karenying'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <GoMarkGithub />
+        </a>
+      </Tooltip>
+    </div>
+    <div className={`${styles['contacts__email']} ${styles['contacts__link']}`}>
+      <Tooltip title="Email" aria-label="email" placement="top">
+        <a
+          href='mailto:karenying7@gmail.com'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <IoMdMail size='1.1rem'/>
+        </a>
+      </Tooltip>
+    </div>
+    <div className={`${styles['contacts__linkedin']} ${styles['contacts__link']}`}>
+      <Tooltip title="LinkedIn" aria-label="linkedin" placement="top">
+        <a
+          href='https://www.linkedin.com/in/kyying/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaLinkedin size='1.1rem'/>
+        </a>
+      </Tooltip>
+    </div>
+    <div className={`${styles['contacts__twitter']} ${styles['contacts__link']}`}>
+      <Tooltip title="Twitter" aria-label="twitter" placement="top">
+        <a
+          href='https://twitter.com/karen_ying_'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <AiFillTwitterCircle size='1.2rem'/>
+        </a>
+      </Tooltip>
+    </div>
   </div>
 );
 
