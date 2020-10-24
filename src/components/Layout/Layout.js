86d2fdaa43260db5d5803f61c32941598448921d
Karen Ying <karenying7@gmail.com>
2020-10-24 14:54:38 -0400

    Social image oops

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import styles from './Layout.module.scss';
 
-const Layout = ({ children, title, description }) => {
+const Layout = ({ children, title, description, socialImage }) => {
+  const metaImage = socialImage || '/preview.png';
+  const metaImageUrl = 'https://www.blog.karenying.com' + metaImage;
+
   return (
     <div className={styles.layout}>
       <Helmet>
@@ -11,17 +14,11 @@ const Layout = ({ children, title, description }) => {
         <meta name='description' content={description} />
         <meta property='og:site_name' content={title} />
         <meta property='og:title' content='blog.karenying.com' />
-        <meta
-          property='og:image'
-          content='https://www.blog.karenying.com/preview.png'
-        />
+        <meta property='og:image' content={metaImageUrl} />
         <meta name='twitter:card' content='summary_large_image' />
         <meta name='twitter:title' content={title} />
         <meta name='twitter:description' content={description} />
-        <meta
-          name='twitter:image'
-          content='https://www.blog.karenying.com/preview.png'
-        />
+        <meta name='twitter:image' content={metaImageUrl} />
       </Helmet>
       {children}
     </div>
